8799fbf1c811a79c9388af630a942ce771e2e6df
Sui <yufangcheng1101@gmail.com>
2021-05-10 15:49:05 +1000

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 //載入restaurant list資料
 const restList = require('./models/seeds/restaurant.json')
+const Restaurant = require('./models/restaurant') //// 載入 restaurant model
 
 const app = express()
 const port = 3001
@@ -33,17 +34,21 @@ app.use(express.static('public'))
 // 路由設定
 //restaurant 首頁路由
 app.get('/', (req, res) => {
-  const restaurants = restList.results
-  res.render('index', { restaurants: restaurants })
+  Restaurant.find()// 取出 Restaurant model 裡的所有資料
+    .lean() //// 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(restaurants => res.render('index', { restaurants })) //// 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+  // const restaurants = restList.results
+  // res.render('index', { restaurants: restaurants })
 })
 
 
 //打造show頁面 設定動態路由
 app.get('/restaurants/:restaurant_id', (req, res) =>{
 
-  const restaurants = restList.results.find( item =>  item.id.toString() === req.params.restaurant_id )
+  const restaurant = restList.results.find( item =>  item.id.toString() === req.params.restaurant_id )
 
-  res.render('show', { restaurant: restaurants })
+  res.render('show', { restaurant: restaurant })
 })
 
 //queryString
@@ -51,7 +56,7 @@ app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const trimKeyword = keyword.trim().toLowerCase()
   //search by name  name_en or category
-  const restaurantsSearch = restList.results.filter(restaurant => {
+  const restaurants = restList.results.filter(restaurant => {
     //包含中文名稱 英文名稱 類別等關鍵字搜尋
     return (
       restaurant.name.toLowerCase().includes(trimKeyword) ||
@@ -59,7 +64,7 @@ app.get('/search', (req, res) => {
       restaurant.category.toLowerCase().includes(trimKeyword) 
     )
   })
-  res.render('index', { restaurants: restaurantsSearch, keyword: keyword })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 // start and listen on the Express server
